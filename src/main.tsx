977efaeeca1ba495f2eed7770ee9abf79c1cf2c7
k3y10 <76926074+k3y10@users.noreply.github.com>
2026-06-29 11:17:31 -0600

    Load final MindLaunch image styling layer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 import "./mindlaunch-theme.css";
+import "./mindlaunch-image-fixes.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>,
